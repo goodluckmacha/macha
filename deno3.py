b3192426b361444b8e17ffd19e1d7c63ee8c1264
goodluckmacha <97720851+goodluckmacha@users.noreply.github.com>
2022-02-11 12:16:33 +0300

    Add files via upload

---
+++ deno3.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import streamlit as st
+st.header("celcius to farenheit")
+st.write("temp conversion")
+c=st.number_input("input the temp in celcius")
+st.write("temp in farenheit is",c*9/5+32)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
